9f3a36917840ae5961bcdcc57995d868539483bf
Akbar Ahmadi Saray <akbarsafari00@gmail.com>
2024-05-12 16:51:01 +0330

    .

--- Frameworks/src/Novino.Scheduler.Quartz/Novin/NovinSchedulerExtensions.cs
+++ Frameworks/src/Novino.Scheduler.Quartz/Novin/NovinSchedulerExtensions.cs
@@ -5,30 +5,12 @@ namespace Novin;
 
 public static class NovinSchedulerExtensions
 {
-  public static INovinoBuilder AddScheduler(this INovinoBuilder builder)
+  public static INovinoBuilder AddQuartzScheduler(this INovinoBuilder builder , Action<IServiceCollectionQuartzConfigurator> configurator)
   {
-    
-    builder.Services.AddQuartz(q =>
-    {
-      var types = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes()).Where(x=>x.IsAssignableTo(typeof(IJob)));
-      foreach (var type in types)
-      {
-        q.AddJob(type, new JobKey(type.Name));
-        
-      }
-      q.AddTrigger(c =>
-      {
-        foreach (var type in types)
-        {
-          c.ForJob(type.Name).StartNow().WithSimpleSchedule(v => v.Build());
-        }
-        
-      });
-    });
+    builder.Services.AddQuartz(configurator);
 
     builder.Services.AddQuartzHostedService(options =>
     {
-      // when shutting down we want jobs to complete gracefully
       options.WaitForJobsToComplete = true;
     });
     return builder;
